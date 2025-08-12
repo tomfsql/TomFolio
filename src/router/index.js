def33d2a7495552374f9f6d12531991aa4d944e6
Tom Fasquelle <102672631+fasquelt@users.noreply.github.com>
2025-08-12 16:39:06 +0200

    Update index.js with proper routing

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomePage from '../components/HomePage.vue'
 import AboutPage from '@/components/AboutPage.vue'
 import ProjectsPage from '@/components/ProjectsPage.vue'
 import SkillsPage from '@/components/SkillsPage.vue'
+import BUTSkillsPage from '@/components/BUTSkillsPage.vue'
 
 const router = createRouter({
   history: createWebHistory(process.env.VUE_BASE_URL),
@@ -10,7 +11,8 @@ const router = createRouter({
     { path: '/', name: 'Home', component: HomePage },
     { path: '/about', name: 'À propos de moi', component: AboutPage },
     { path: '/projects', name: 'Projets', component: ProjectsPage },
-    { path: '/skills', name: 'Compétences', component: SkillsPage }
+    { path: '/skills', name: 'Compétences techniques', component: SkillsPage },
+    { path: '/BUT', name: 'Compétences du BUT', component: BUTSkillsPage }
   ],
   scrollBehavior(to) {
     if (to.hash) {
