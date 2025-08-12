d4ffea1a1397390ba64166dcdc7aba965b41488c
Tom Fasquelle <102672631+fasquelt@users.noreply.github.com>
2025-08-12 09:46:44 +0200

    Added imports for footer

--- src/App.vue
+++ src/App.vue
@@ -10,16 +10,17 @@
     <RouterView></RouterView>
   </div>
 
-  <Footer></Footer>
+  <Footer>
 </template>
 
 <script>
  import { RouterLink, RouterView } from 'vue-router'
+ import Footer from "./components/Footer.vue";
 
 export default {
   name: 'App',
   components: {
-      RouterLink, RouterView
+      RouterLink, RouterView,Footer 
   }
 }
 </script>
